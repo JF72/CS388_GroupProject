e3b5b87cf2f933b73d16359ff2cb48f6f41cc478
Carlos <cr46@njit.edu>
2025-04-15 20:49:33 -0400

    Pushing initial taro login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
