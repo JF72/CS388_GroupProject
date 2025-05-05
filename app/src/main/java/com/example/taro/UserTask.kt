5f7c899044aa3fa4f390f1ebb20468bfa381cfe3
Rafael Abreu <abreurafa22@gmail.com>
2025-05-05 00:01:13 -0400

    Created serializable class for UserTask

--- app/src/main/java/com/example/taro/UserTask.kt
+++ app/src/main/java/com/example/taro/UserTask.kt
@@ -1,13 +1,17 @@
 package com.example.taro
 
-
+import com.bumptech.glide.Priority
+import java.time.LocalDateTime
 
 
 data class UserTask(
-    val id: String = "",
-    val name: String = "",
-    val description: String = "",
-    val dueDate: Long? = null,
-    val points: Int = 0,
-    val completed: Boolean = false
-)
+    val name: String? = "",
+    val description: String? = "",
+    val difficulty: Int?,
+    val priority: Int?,
+    val urgency : Int?,
+    val dueDate : LocalDateTime?,
+    val isCompleted: Boolean?,
+    val taroScore: Double? ,
+
+) : java.io.Serializable
